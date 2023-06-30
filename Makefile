148def532f1ac61bc0634f8e637648b2884219a7
Ruslan Maistrenko <rmaistrenko@verenia.com>
2023-06-30 13:54:23 +0300

    add transaction and tests

--- Makefile
+++ Makefile
@@ -14,7 +14,7 @@ migratedown:
 	migrate -path db/migration -database "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable" -verbose down
 
 sqlc:
-	sqlc generate
+	docker run --rm -v ${pwd}:/src -w /src kjconroy/sqlc generate
 
 test:
 	go test -v -cover ./...
--- db/query/account.sql
+++ db/query/account.sql
@@ -9,6 +9,13 @@ FROM account
 WHERE id = $1
 LIMIT 1;
 
+-- name: GetAccountForUpdate :one
+SELECT *
+FROM account
+WHERE id = $1
+LIMIT 1
+FOR NO KEY UPDATE;
+
 -- name: ListAccounts :many
 SELECT *
 FROM account
@@ -19,6 +26,12 @@ LIMIT $1 OFFSET $2;
 UPDATE account
 set balance = $2
 WHERE id = $1
+RETURNING *;
+
+-- name: AddAccountBalance :one
+UPDATE account
+SET balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
 RETURNING *;
 
 -- name: DeleteAccount :exec
